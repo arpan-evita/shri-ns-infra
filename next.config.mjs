054c417fea139964ec78d273845f17dcc956aa10
arpan-evita <arpansadhu13@gmail.com>
2026-03-17 12:12:54 +0530

    feat: convert project to Single Page Application (SPA) using Vite

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['images.unsplash.com', 'bpdvcyceybngwrbfnubr.supabase.co'],
-  },
-};
-
-export default nextConfig;
